9d632aea41ebdd2842e1adf89de83c91aa28c807
Sebastián Velásquez <sebas99peru@gmail.com>
2022-08-05 20:08:50 -0500

    app.js: se añadio logo de marvel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import logoMarvel from './images/logo-marvel.png';
 
 function App() {
   return (
     <div className='App'>
-      MARVEL
+
+      <section className='app__container-logo'>
+        <img src={logoMarvel} alt='Logo marvel' className='app__logo' />
+      </section>
+      
+      <section className='app__container-main'>
+        <h1 className='main__title'>Listado de COMICS</h1>
+      </section>
+
     </div>
   );
 }
